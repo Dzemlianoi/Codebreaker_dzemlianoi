4ba9289ec7e4481cc8c1eff90613e03d21ae03e3
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-12-15 12:03:49 +0200

    game redone

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,71 +1,68 @@
 module Codebreaker
   class Game
     attr_accessor :current_code, :secret_code, :difficulties, :difficulty,
-                  :attempts_left, :hints_left, :hint_code_digits, :name
+                  :attempts_left, :hints_left, :hint_code_digits, :name,
+                  :attempts_array, :hints_array
 
     def initialize
-      self.difficulties = Loader.load('difficulties')
-      self.secret_code = generate_secret_code
+      @difficulties = Loader.load('difficulties')
+      @attempts_array, @hints_array = []
     end
 
-    def asign_game_options (name, difficulty)
-      self.name = name
-      self.difficulty= difficulty
-      self.hints_left = difficulty_info[:hints]
-      self.attempts_left = difficulty_info[:attempts]
-      self.hint_code_digits = secret_code.clone
+    def init_game_options (name, difficulty)
+      @name = name
+      @difficulty= difficulty.to_sym
+      @hints_left = difficulty_info[:hints]
+      @attempts_left = difficulty_info[:attempts]
+      @secret_code = generate_secret_code
+      @hint_code_digits = secret_code.clone
+      to_h
+    end
+
+    def init_started_game_options(options)
+      options.each do |k,v|
+        instance_variable_set("@#{k}", v)
+      end
+      to_h
     end
 
     def get_hint
       return unless hints_left?
-      self.hints_left -= 1
-      get_hint_digit
+      @hints_left -= 1
+      hint = get_hint_digit
+      @hints_array.push(hint)
     end
 
     def hints_left?
-      self.hints_left > 0
+      @hints_left > 0
     end
 
     def code_operations(current_code)
-      self.current_code = current_code
-      self.attempts_left -= 1
-      marking_result
+      @current_code = current_code
+      @attempts_left -= 1
+      @attempts_array.push(@current_code)
     end
 
     def win?
-      current_code == secret_code
+      @current_code == @secret_code
     end
 
-    def to_h
-      {
-          name: name,
-          difficulty: difficulty,
-          attempts: difficulty_info[:attempts],
-          attempts_left: attempts_left,
-          hints: difficulty_info[:hints],
-          hints_left: hints_left,
-          secret_code: secret_code
-      }
+    def loose?
+      @attempts_left < 0
     end
 
-    private
-
-    def generate_secret_code
-      Array.new(4) { rand(0..6)}.join
+    def last_chance
+      @attempts_left == 1
     end
 
-    def get_hint_digit
-      hint_code_digits.slice!(rand(hint_code_digits.size))
+    def difficulty_info
+      difficulties[@difficulty]
     end
 
-    def attempts_left?
-      attempts_left > 0
-    end
-
-    def marking_result
+    def marking_result(code)
       answer = ''
-      secret_copy = secret_code.split('')
-      current_copy = current_code.split('')
+      secret_copy = @secret_code.split('')
+      current_copy = code.split('')
       secret_copy.each_with_index do |val, key|
         next unless val == current_copy[key]
         current_copy[key], secret_copy[key] = nil
@@ -79,12 +76,32 @@ module Codebreaker
         answer << '-'
         secret_copy[secret_copy.find_index(digit)] = nil
       end
-
       answer
     end
 
-    def difficulty_info
-      difficulties[self.difficulty]
+    def to_h
+      {
+          name: @name,
+          difficulty: @difficulty,
+          attempts_left: @attempts_left,
+          hints_left: @hints_left,
+          secret_code: @secret_code,
+          hints_array: @hints_array,
+          attempts_array: @attempts_array,
+          hint_code_digits: @hint_code_digits
+
+      }
+    end
+
+    private
+
+    def generate_secret_code
+      Array.new(4) { rand(0..6)}.join
+    end
+
+    def get_hint_digit
+      @hint_code_digits.slice!(rand(@hint_code_digits.size))
     end
   end
 end
+
